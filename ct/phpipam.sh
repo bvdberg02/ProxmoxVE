744e1675d1b6d8b186fc324b52d20e33832e551f
bvdberg01 <s1162113@student.windesheim.nl>
2024-12-01 00:36:33 +0100

    Update phpipam.sh

--- ct/phpipam.sh
+++ ct/phpipam.sh
@@ -90,4 +90,4 @@ description
 
 msg_ok "Completed Successfully!\n"
 echo -e "${APP} should be reachable by going to the following URL.
-         ${BL}http://${IP}:9000${CL} \n"
+         ${BL}http://${IP}${CL} \n"
